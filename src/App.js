54d7e1ce1c7e3a19616d4aee8ee881fb48d9ac3f
Darren <darren.doyler@gmail.com>
2020-11-30 21:49:33 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,251 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="Div-header">
+        <header className="App-header">
+          Darren <b>Doyle</b>
+        </header> 
+        <div className="Header-details"><b><a href="https://github.com/swizkhalifaa">github.com/swizkhalifaa</a></b> || <b><a href="mailto: darren.doyle3710@gmail.com">darren.doyle3710@gmail.com</a></b> || +353 0830032583</div>
+      </div>
+      
+      <div className="Bottom-div-behavior">
+        <div className="Left-div">
+          <div className="Div-compartment">
+          <div className="Div-compartment">
+        <header className="Bottom-header">
+          EDUCATION
+        </header> 
+        </div>
+        <header className="Bottom-header-bold">
+          DUBLIN BUSINESS SCHOOL
+        </header> 
+        <header className="Bottom-header-light">
+          BSc in Computing
+        </header>
+        <header className="Bottom-header-light-small">
+        <b>GPA: 1.1 (First Class Honours)</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          2016-2020 || Dublin, Ireland
+        </header>
+        </div>
+
+
+        <header className="Bottom-header-light">
+          MODULES COVERED:
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Object-Orientated Programming</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Programming Fundamentals</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Algorithms and Data Structures</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Web Development</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Big Data</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Distributed Systems</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Databases</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Operating Systems</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Computer Architecture</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Computer Security</b>
+        </header>
+
+
+
+
+        <div className="Div-compartment">
+        <header className="Bottom-header-bold">
+          CASTLEKNOCK COMMUNITY COLLEGE
+        </header> 
+        <header className="Bottom-header-light">
+          Leaving Certificate
+        </header>
+        <header className="Bottom-header-light-small">
+          <b>Passed Higher-Level Mathematics</b>
+        </header>
+        <header className="Bottom-header-light-small">
+          2011-2016 || Dublin, Ireland
+        </header>
+        </div>
+      
+
+
+        <div className="Subject-compartment">
+        <div className="Div-compartment">
+        <div className="Div-compartment">
+        <header className="Bottom-header">
+          EXPERIENCE
+        </header> 
+        </div>
+        <header className="Bottom-header-bold">
+          OCUCO LTD  <text className="Bottom-header-light-small"> | PROFESSINAL SERVICES ENGINEER</text>
+        </header> 
+        <header className="Bottom-header-light">
+          Internship
+        </header>
+        <header className="Bottom-header-light-small">
+          09/18 - 03/19 || Dublin, Ireland
+        </header>
+       </div>
+        <header className="Bottom-header-light">
+          WORK DETAILS:
+        </header>
+        <div className="Div-compartment">
+        <header className="Bottom-header-light-small">
+          - Provided first line support to customers in United States and Europe
+        </header>
+        <header className="Bottom-header-light-small">
+          - Ran SQL queries to resolve irregularities in customers databases
+        </header>
+        <header className="Bottom-header-light-small">
+          - Liaised with the development team to troubleshoot critical and urgent issues
+        </header>
+        <header className="Bottom-header-light-small">
+          - Testing of new software
+        </header>
+        <header className="Bottom-header-light-small">
+          - Record all issues in JIRA
+        </header>
+        </div>
+        </div>
+        </div>
+      {/* other side */}
+        <div className="Right-div">
+        <div className="Div-compartment">
+        <div className="Div-compartment">
+        <header className="Bottom-header">
+          TECHNICAL SKILLS
+        </header> 
+        </div>
+        </div>
+        <div className="Div-tech">
+          <div className="Div-language">
+          <header className="Bottom-header-bold">
+          PROGRAMMING
+            </header> 
+            <header className="Bottom-header-light">
+              - Java
+            </header>
+            <header className="Bottom-header-light">
+              - C#
+            </header>
+            <header className="Bottom-header-light">
+              - Python
+            </header>
+            <header className="Bottom-header-light">
+              - Kotlin
+            </header>
+            <header className="Bottom-header-light">
+              - Javascript
+            </header>
+            <header className="Bottom-header-light">
+              - ReactJS
+            </header>
+            <header className="Bottom-header-light">
+              - HTML5/CSS
+            </header>
+            <header className="Bottom-header-light">
+              - Android
+            </header>
+            </div>
+
+
+            <div className="Div-other">
+            <header className="Bottom-header-bold">
+              DATABASES
+            </header> 
+            <header className="Bottom-header-light">
+              - SQL
+            </header>
+            <header className="Bottom-header-light">
+              - MongoDB
+            </header>
+            <div className="Div-compartment">
+            <header className="Bottom-header-bold">
+              IDEs
+            </header> 
+            <header className="Bottom-header-light">
+              - Visual Studio
+            </header>
+            <header className="Bottom-header-light">
+              - VS Code
+            </header>
+            <header className="Bottom-header-light">
+              - Android Studio
+            </header>
+            <header className="Bottom-header-light">
+              - Kotlin
+            </header>
+            </div>
+            </div>
+        </div>
+        
+        <div className="Subject-compartment">
+        <div className="Div-compartment">
+        <div className="Div-compartment">
+        <header className="Bottom-header">
+          PORTFOLIO
+        </header> 
+        </div>
+        <header className="Bottom-header-light">
+          <a href='https://github.com/swizkhalifaa/College-Capstone-Project---Android-Application-Kotlin-'><b>CAPSTONE ANDROID APPLICATION </b></a>
+           | Kotlin + Android
+        </header>
+        <div className="Div-project">
+        <header className="Bottom-header-light-small">
+        Final year college project that involved learning Android development with no prior knowledge and writing an application in Kotlin which also had to be learned. Social media application with a Firebase backend allowing chatting and a 'matching' system.
+        </header>
+        </div>
+        <header className="Bottom-header-light">
+        <a href='https://github.com/swizkhalifaa/Distributed-System-Project-C-'><b> DISTRIBUTED CHAT ROOM </b></a>
+        | C# + JavaScript
+        </header>
+        <div className="Div-project">
+        <header className="Bottom-header-light-small">
+        A distributed web chat application which demonstrates concepts such as fault tolerance, concurrency and scalability using ASP.NET with C# and JavaScript.
+        </header>
+        </div>
+        <header className="Bottom-header-light">
+         <a href='https://github.com/swizkhalifaa/WebScraperPython'><b> FOOTBALL WEB SCRAPER </b></a>
+        | Python
+        </header>
+        <div className="Div-project">
+        <header className="Bottom-header-light-small">
+        A data processing pipeline system written in Python which scrapes browser HTML to obtain dynamic football statistics in realtime.
+        </header>
+        </div>
+        <header className="Bottom-header-light">
+        <a href='https://github.com/swizkhalifaa/React-First-Website'><b> SPOTIFY SOCIAL NETWORK </b></a>
+        | ReactJS + JavaScript
+        </header>
+        <div className="Div-project">
+        <header className="Bottom-header-light-small">
+        An ambitious summer project post-college to learn JavaScript and ReactJS and make a web application with Spotify functionality through REST API calls.
+        </header>
+        </div>
+        </div>
+        </div>
+        </div>
+      </div>
+    </>
   );
 }
 
